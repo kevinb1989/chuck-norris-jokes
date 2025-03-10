78fb9498ad107f2fa3045d18cddb7afa576c3fc5
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2025-03-10 18:35:01 +1100

    20. Using a Joke API.

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,23 +2,31 @@
 
 namespace Kevinb1989\ChuckNorrisJokes;
 
+use GuzzleHttp\Client;
+
 class JokeFactory
 {
+    const API_ENDPOINT = 'https://api.chucknorris.io/jokes/random';
+
     protected $jokes = [
         "Chuck Norris doesn't read books. He stares them down until he gets the information he wants.",
         'Time waits for no man. Unless that man is Chuck Norris.',
         'If you spell Chuck Norris in Scrabble, you win. Forever.',
     ];
 
-    public function __construct(?array $jokes = null)
+    protected Client $client;
+
+    public function __construct(Client $client = null)
     {
-        if ($jokes) {
-            $this->jokes = $jokes;
-        }
+        $this->client = $client ?: new Client();
     }
 
     public function getRandomJoke(): string
     {
-        return $this->jokes[array_rand($this->jokes)];
+        $response = $this->client->get(self::API_ENDPOINT);
+
+        $content = json_decode($response->getBody()->getContents());
+
+        return $content->value;
     }
 }
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,6 +2,10 @@
 
 namespace Kevinb1989\ChuckNorrisJokes\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use Kevinb1989\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -9,19 +13,28 @@ class JokeFactoryTest extends TestCase
 {
     public function test_it_returns_a_random_joke()
     {
-        $jokes = new JokeFactory([
-            'This is a joke',
+        $mock = new MockHandler([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                json_encode([
+                    'categories' => [],
+                    'created_at' => '2020-01-05 13:42:19.576875',
+                    'icon_url' => 'https://api.chucknorris.io/img/avatar/chuck-norris.png',
+                    'id' => '7Po2MPWaTdCoRD0eD8g6TQ',
+                    'updated_at' => '2020-01-05 13:42:19.576875',
+                    'url' => 'https://api.chucknorris.io/jokes/7Po2MPWaTdCoRD0eD8g6TQ',
+                    'value' => 'Chuck Norris urinates while doing a hand-stand on the toilet seat.',
+                ]),
+            ),
         ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(compact('handler'));
+
+        $jokes = new JokeFactory($client);
         $joke = $jokes->getRandomJoke();
 
-        $this->assertSame('This is a joke', $joke);
-    }
-
-    public function test_it_returns_a_predefined_joke()
-    {
-        $jokes = new JokeFactory();
-        $joke = $jokes->getRandomJoke();
-
-        $this->assertNotEmpty($joke);
+        $this->assertSame('Chuck Norris urinates while doing a hand-stand on the toilet seat.', $joke);
     }
 }
