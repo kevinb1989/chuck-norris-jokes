925e8183080c8ee802b62536004aedd66de520e0
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2025-03-15 19:38:55 +1100

    23. Create a Custom Command.

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,11 +3,17 @@
 namespace Kevinb1989\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
+use Kevinb1989\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ChuckNorrisJoke::class
+            ]);
+        }
     }
 
     public function register()
---
+++ src/Console/ChuckNorrisJoke.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kevinb1989\ChuckNorrisJokes\Console;
+
+use Illuminate\Console\Command;
+use Kevinb1989\ChuckNorrisJokes\Facades\ChuckNorris;
+
+class ChuckNorrisJoke extends Command
+{
+    protected $signature = 'chuck-norris';
+
+    protected $description = 'Output a funny Chuck Norris joke.';
+
+    public function handle()
+    {
+        $this->info(ChuckNorris::getRandomJoke());
+    }
+}
